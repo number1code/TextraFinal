431401d8d2b4e7a8b26bc8906958092739c61a7a
Eduardo Nunez <mrnunez87@gmail.com>
2025-10-04 15:09:15 -0500

    Update to the workflow, we now use whisper ai to get a json that has a per word time stamp. Feed this to your favorite LLM to cook up another json for building LyricLine objects that are automatically read in time with the music, examples included

--- core/src/main/java/com/eduardo/nunez/TextraFinal/MyGame.java
+++ core/src/main/java/com/eduardo/nunez/TextraFinal/MyGame.java
@@ -1,13 +1,31 @@
 package com.eduardo.nunez.TextraFinal;
 
 import com.badlogic.gdx.Game;
+import com.github.tommyettinger.textra.TypingConfig;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class MyGame extends Game {
     @Override
     public void create() {
+        //Useful combined effects saved into variables
+        TypingConfig.GLOBAL_VARS.put("DECAY", "{SICK=0.5;0.8}{COLOR=dull weak green}");
+        TypingConfig.GLOBAL_VARS.put("ENDDECAY", "{ENDSICK}{CLEARCOLOR}");
+        //Ashes in the air
+        TypingConfig.GLOBAL_VARS.put("ASH_FALL", "{FADE=0;1;2}{WIND=0.05;0.05;4;0.5}{COLOR=pale weak gray}");
+        TypingConfig.GLOBAL_VARS.put("ENDASH_FALL", "{CLEARCOLOR}{ENDWIND}{ENDFADE}");
+
+        TypingConfig.GLOBAL_VARS.put("FLICKER_LIGHT", "{JOLT=1;1;0.3;0.1;black;pale yellow}");
+        TypingConfig.GLOBAL_VARS.put("ENDFLICKER_LIGHT", "{ENDJOLT}");
+
+        TypingConfig.GLOBAL_VARS.put("SMOKE_LUNGS", "{SICK=0.5;0.8}{WAVE=0.05;3;0.5}{COLOR=darkest gray}");
+        TypingConfig.GLOBAL_VARS.put("ENDSMOKE_LUNGS", "{CLEARCOLOR}{ENDWAVE}{ENDSICK}");
+
+        TypingConfig.GLOBAL_VARS.put("GHOST_WHISPER", "{FADE=0.2;0.8;1.5}[%80][pale cyan]");
+        TypingConfig.GLOBAL_VARS.put("ENDGHOST_WHISPER", "[%]{CLEARCOLOR}{ENDFADE}");
+
         //setScreen(new VfxProductionSquareScreen(this));
-        setScreen(new VfxProductionScreen(this));
+        //setScreen(new VfxProductionScreen(this));
+        setScreen(new AutoProductionScreen(this));
     }
 
 }
